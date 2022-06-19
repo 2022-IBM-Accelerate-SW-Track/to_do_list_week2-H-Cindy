0bd99a876994c6ed40ff6e93736dbd664b8999c4
Hou <hou@Hous-MacBook-Air.local>
2022-06-18 20:37:22 -0500

    finish reqs for week 2

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,33 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import headshot from "../assets/headshot.jpg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+         <div>
+            <div class="split left">
+              <div className="centered">
+                <img 
+                  className="profile_image"
+                  src={headshot}
+                  alt="Profile Pic"
+                  ></img>
+              </div>
+            </div>
+            <div className="split right">
+              <div className="centered">
+                <div className="name_title">Cindy Hou</div>
+                <div className="brief_description">
+                  <p>I am a rising junior at Cornell University studying Statistics and Information Science.</p>
+                  <p>My interests include machine learning and data science.</p>
+                  <p>Outside of academics, I also enjoy hiking, art, and reading.</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
       </div>
-    );
+    )
   }
 }
